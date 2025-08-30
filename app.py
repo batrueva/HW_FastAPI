7c9599eec2a24507760a8d99adf8932d08b98a26
batrueva <ekaterinabatrueva@gmail.com>
2025-08-30 22:04:03 +0300

    30/08/2025

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
-"""
-Домашнее задание №5
-Первое веб-приложение
+from fastapi import FastAPI
 
-- в модуле `app` создайте базовое FastAPI приложение
-- создайте обычные представления
-  - создайте index view `/`
-  - добавьте страницу `/about/`, добавьте туда текст, информацию о сайте и разработчике
-  - создайте базовый шаблон (используйте https://getbootstrap.com/docs/5.0/getting-started/introduction/#starter-template)
-  - в базовый шаблон подключите статику Bootstrap 5 (подключите стили), примените стили Bootstrap
-  - в базовый шаблон добавьте навигационную панель `nav` (https://getbootstrap.com/docs/5.0/components/navbar/)
-  - в навигационную панель добавьте ссылки на главную страницу `/` и на страницу `/about/` при помощи `url_for`
-  - добавьте новые зависимости в файл `requirements.txt` в корне проекта
-    (лучше вручную, но можно командой `pip freeze > requirements.txt`, тогда обязательно проверьте, что туда попало, и удалите лишнее)
-- создайте api представления:
-  - создайте api router, укажите префикс `/api`
-  - добавьте вложенный роутер для вашей сущности (если не можете придумать тип сущности, рассмотрите варианты: товар, книга, автомобиль)
-  - добавьте представление для чтения списка сущностей
-  - добавьте представление для чтения сущности
-  - добавьте представление для создания сущности
-"""
+import uvicorn
+
+from routers.movie_router import router as movie_router
+from routers.index_routers import router as index_routers
+
+app = FastAPI()
+
+app.include_router(index_routers)
+app.include_router(movie_router)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
