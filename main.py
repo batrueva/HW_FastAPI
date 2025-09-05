532d40934483b9cd8f02d1c7fc5100671082de4e
batrueva <ekaterinabatrueva@gmail.com>
2025-09-05 19:31:30 +0300

    05/09/2025

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI
 
 import uvicorn
 
-from routers.movie_router import router as movie_router
+from routers.book_router import router as book_router
 from routers.index_routers import router as index_routers
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
-
+app.mount("/images", StaticFiles(directory="images", packages=None, html=False, check_dir=True), name="images" )
 app.include_router(index_routers)
-app.include_router(movie_router)
-
+app.include_router(book_router)
 
 if __name__ == '__main__':
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
